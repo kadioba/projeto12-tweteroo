1bb0ef8fd66a50961d8bd5ca43b1d36c47b29d14
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-04-08 18:15:34 -0300

    Versao com post tweets funcional

--- src/app.js
+++ src/app.js
@@ -8,12 +8,23 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/sign-up", (req, res) => {
-    console.log(usuarios)
     const { username, avatar } = req.body;
     usuarios.push({ username, avatar });
     res.send("OK")
     console.log(usuarios)
+})
 
+app.post("/tweets", (req, res) => {
+    console.log(tweets)
+    const { username, tweet } = req.body;
+    const userCadastrado = usuarios.find(usuario => usuario.username === username);
+    if (!userCadastrado) {
+        res.send("UNAUTHORIZED");
+        return;
+    }
+    tweets.push({ username, tweet });
+    res.send("OK")
+    console.log(tweets)
 })
 
 app.listen(5000);

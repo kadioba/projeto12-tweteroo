3fda72103be0db05a6cfb2b6d895e5d24d7ba5e0
Vinicius Costa Predebon <kadiobaoff@gmail.com>
2023-04-08 21:30:37 -0300

    Versao com correcao no get tweets

--- src/tweets.js
+++ src/tweets.js
@@ -1,24 +1,61 @@
 const tweets = [
-    {
-        username: "bobesponja",
-        tweet: "Eu amo hambúrguer de siri!"
-    },
-    {
-        username: "bobesponja",
-        tweet: "Eu amo hambúrguer de siri!"
-    },
-    {
-        username: "user3",
-        tweet: "Antepenultimo tweetr nativo"
-    },
-    {
-        username: "user2",
-        tweet: "Penultimo Tweet nativo"
-    },
-    {
-        username: "user1",
-        tweet: "Ultimo tweet nativo"
-    },
+    /*     {
+            username: "bobesponja",
+            tweet: "Eu amo hambúrguer de siri!"
+        },
+        {
+            username: "bobesponja",
+            tweet: "Eu amo hambúrguer de siri!"
+        },
+        {
+            username: "bobesponja",
+            tweet: "Eu amo hambúrguer de siri!"
+        },
+        {
+            username: "bobesponja",
+            tweet: "Eu amo hambúrguer de siri!"
+        },
+        {
+            username: "user3",
+            tweet: "5"
+        },
+        {
+            username: "user2",
+            tweet: "6"
+        },
+        {
+            username: "user1",
+            tweet: "7"
+        },
+        {
+            username: "user1",
+            tweet: "8"
+        },
+        {
+            username: "user1",
+            tweet: "9"
+        },
+        {
+            username: "user1",
+            tweet: "10"
+        },
+        {
+            username: "user1",
+            tweet: "11"
+        },
+        {
+            username: "user1",
+            tweet: "12"
+        },
+        {
+            username: "user1",
+            tweet: "13"
+        },
+        {
+            username: "user1",
+            tweet: "14"
+        }
+     */
 ]
 
 export default tweets;
--- src/usuarios.js
+++ src/usuarios.js
@@ -1,8 +1,8 @@
 const usuarios = [
-    { username: 'bobesponja', avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" },
-    { username: 'user1', avatar: "foto user 1" },
-    { username: 'user2', avatar: "foto user 2" },
-    { username: 'user3', avatar: "foto user 3" }
+    /*     { username: 'bobesponja', avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png" },
+        { username: 'user1', avatar: "foto user 1" },
+        { username: 'user2', avatar: "foto user 2" },
+        { username: 'user3', avatar: "foto user 3" } */
 ]
 
 export default usuarios;
